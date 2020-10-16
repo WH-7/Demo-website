ed0f027ea47ee981a14e097dd7d730ab934b0e02
WH-7 <wh7webdev@gmail.com>
2020-10-16 19:23:33 +0530

    Banner
    banner setup +  html + css + js with GSAP + responsive

---
+++ JS/app.js
@@ -0,0 +1,51 @@
+const navBtn = document.querySelector('.nav-btn');
+const navOpen = document.querySelector('.nav-open');
+
+const tl = new TimelineLite(
+{
+  paused: true,
+  reversed: true,
+  }
+);
+
+
+tl.to('.banner', 1, {
+ width : '60%',
+ ease: Power2.easeOut,
+
+})
+.to('nav', 1, {
+ height: '100%',
+ ease: Power2.easeOut,
+}, '-=0.5')
+
+.fromTo('.nav-open', 0.5, {
+ 
+ opacity: 0,
+left: -50,
+ ease: Power2.easeOut
+},{
+ opacity: 1,
+ left: 0,
+
+onComplete: function(){
+navOpen.style.pointerEvents = 'auto'
+console.log('done')
+} 
+
+})
+
+function toggleTween(tween){
+ tween.reversed()? tween.play(): tween.reverse()
+}
+
+
+
+navBtn.addEventListener('click', () => {
+ if(tl.isActive()){
+  e.preventDefaul();
+  e.stopImmediatePropagation();
+  return false;
+ }
+ toggleTween(tl)
+})
