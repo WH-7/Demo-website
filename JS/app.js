29e3e2f320b4cc30e5cd9d16bac9b8fb43beba4e
WH-7 <wh7webdev@gmail.com>
2020-10-28 18:21:08 +0530

    about and design pages added

--- JS/app.js
+++ JS/app.js
@@ -1,60 +1,58 @@
-const navBtn = document.querySelector('.nav-btn');
-const navOpen = document.querySelector('.nav-open');
+const navBtn = document.querySelector(".nav-btn");
+const navOpen = document.querySelector(".nav-open");
 
-const tl = new TimelineLite(
-{
+const tl = new TimelineLite({
   paused: true,
   reversed: true,
+});
+
+tl.to(".banner", 1, {
+  width: "60%",
+  ease: Power2.easeOut,
+})
+  .to(
+    "nav",
+    1,
+    {
+      height: "100%",
+      ease: Power2.easeOut,
+    },
+    "-=0.5"
+  )
+
+  .fromTo(
+    ".nav-open",
+    0.5,
+    {
+      opacity: 0,
+      left: -50,
+      ease: Power2.easeOut,
+    },
+    {
+      opacity: 1,
+      left: 0,
+
+      onComplete: function () {
+        navOpen.style.pointerEvents = "auto";
+        console.log("done");
+      },
+    }
+  );
+
+function toggleTween(tween) {
+  tween.reversed() ? tween.play() : tween.reverse();
+}
+
+navBtn.addEventListener("click", () => {
+  if (tl.isActive()) {
+    e.preventDefaul();
+    e.stopImmediatePropagation();
+    return false;
   }
-);
+  toggleTween(tl);
+});
 
-
-tl.to('.banner', 1, {
- width : '60%',
- ease: Power2.easeOut,
-
-})
-.to('nav', 1, {
- height: '100%',
- ease: Power2.easeOut,
-}, '-=0.5')
-
-.fromTo('.nav-open', 0.5, {
- 
- opacity: 0,
-left: -50,
- ease: Power2.easeOut
-},{
- opacity: 1,
- left: 0,
-
-onComplete: function(){
-navOpen.style.pointerEvents = 'auto'
-console.log('done')
-} 
-
-})
-
-function toggleTween(tween){
- tween.reversed()? tween.play(): tween.reverse()
-}
-
-
-
-navBtn.addEventListener('click', () => {
- if(tl.isActive()){
-  e.preventDefaul();
-  e.stopImmediatePropagation();
-  return false;
- }
- toggleTween(tl)
-})
-
-
-
-
-
-const section = document.querySelectorAll('#section path');
-for (let i = 0; i < section.length; i++){
-console.log(`letter${i} : ${section[i].getTotalLength()}`)
-}
+// const section = document.querySelectorAll('#section path');
+// for (let i = 0; i < section.length; i++){
+// console.log(`letter${i} : ${section[i].getTotalLength()}`)
+// }
